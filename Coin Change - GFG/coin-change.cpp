4f2af8951e1528664fa626b4b91bb695cba75741
mrsaha <99049590+MrUchihaObito@users.noreply.github.com>
2022-10-25 14:42:31 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -8,53 +8,30 @@ class Solution {
     long long int count(int coins[], int N, int sum) {
 
         // code here.
-        long long int dp[sum+1][N+1];
-        for(int i=0;i<=N;i++)
-        {
-            dp[0][i]=1;
-        }
-        for(int i=1;i<=sum;i++)
-        {
-            dp[i][0]=0;
-        }
-       // dp[0][0]=0;
-        for(int i=1;i<=sum;i++)
-        {
-            for(int j=1;j<=N;j++)
-            {
-                dp[i][j]=dp[i][j-1];
-                
-                if(coins[j-1]<=i)
-                {
-                    dp[i][j]+=dp[i-coins[j-1]][j];
-                }
-                
-            }
-        }
-        return dp[sum][N];
-        
-        
-      /*  vector<vector<long long int>>dp(m+1,vector<long long int>(n+1,0));
-        for(int i=0;i<=n;i++)
+        long long dp[N+1][sum+1];
+        for(int i=0;i<sum+1;i++)
         {
             dp[0][i]=0;
         }
-        for(int i=0;i<=m;i++)
+        for(int i=1;i<N+1;i++)
         {
             dp[i][0]=1;
+            
         }
         
-        
-        for(int i=1;i<=m;i++)
+        for(int i=1;i<=N;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<=sum;j++)
             {
                 dp[i][j]=dp[i-1][j];
-                if(S[i-1]<=j)
-                dp[i][j]+=dp[i][j-S[i-1]];
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]+=dp[i][j-coins[i-1]];
+                }
             }
         }
-        return dp[m][n];*/
+        return dp[N][sum];
+        
     }
 };
 
